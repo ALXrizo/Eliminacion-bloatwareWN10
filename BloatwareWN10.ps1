121eb4c1fb5e963435b7400166e5d5e0f9a3cb13
ALXrizo <69232444+ALXrizo@users.noreply.github.com>
2021-03-31 19:07:52 -0600

    .

---
+++ BloatwareWN10.ps1
@@ -0,0 +1,17 @@
+Get-Appxpackage Microsoft.549981c3f5f10 | remove-appxpackage  ###### Cortana ####
+Get-AppxPackage *windowsstore* | Remove-AppxPackage                     ###### Tienda de windows ####
+Get-Appxpackage Microsoft.MixedReality.Portal | Remove-AppxPackage  ##### Portal de realidad mixta ####
+Get-Appxpachage Microsoft.WindowsAlarms | Remove-AppPackage               ######## Alarma #####
+Get-AppxPackage Microsoft.WindowsCalculator | Remove-AppPackage           #######Calculadora #####
+Get-AppPackage Microsoft.WindowsCamera | Remove-AppPackage                ###### Camara ######
+Get-AppPackage Microsoft.ZuneMusic | Remove-AppPackage                    ###### Grove Music ######
+Get-AppPackage Microsoft.Windowscommunicationsapps | Remove-AppPackage    ###### Calendario y correo #####
+Get-AppPackage Microsoft.WindowsMaps | Remove-AppPackage                  ###### Mapas ######
+Get-AppPackage Microsoft.Zunevideo | Remove-AppPackage                    ###### Movies & TV #######
+Get-AppPackage Microsoft.Office.OneNote | Remove-AppPackage               ###### OneNote ####
+Get-AppPackage Microsoft.People | Remove-AppPackage                       ###### Personar #######
+Get-AppPackage Microsoft.Windows.Photos | Remove-AppPackage               ###### Photos ####
+Get-AppPackage Microsoft.WindowsSoundRecorder | Remove-AppPackage         ###### Grabador de sonidos #####
+Get-AppPackage Microsoft.XboxApp | Remove-AppPackage                      ###### Aplicaciones de Xbox #####
+Get-AppxPackage *xboxgaming* | Remove-AppxPackage
+Get-AppxPackage Microsoft.YourPhone | Remove-AppxPackage
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Eliminacion-bloatwareWN10
- 
+----------------------------------------------------------------------------
+
+Script para la desinstalacion del Bloatware que viene instalado por defecto en Windows 10
+
+Aplicaciones:
+
+- Tienda de windows
+
+- Cortana
+
+- Portal de realidad mixta
+
+- Alarma
+
+- Calculadora  (al desinstalar se queda la calculadora por defecto que viene en versiones anteriores de windows)
+
+- Grove Music
+
+- Calendario y Correo
+
+- Mapas
+
+- Movies y TV
+
+- OneNote
+
+- Personas
+
+- Photos
+
+- Grabadora de sonidos
+
+- Aplicaciones de Xbox
+
+- Barra Xbox
+
+- Telefono
+
+------------------------------------------------------------------------------------------------------------------------
+
+Se puede colocar un "#" antes de la linea para omitir la misma y no se ejecute dicha linea
